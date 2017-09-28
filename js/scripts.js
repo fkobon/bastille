4ac4299c9b2f443713df131bd0f386581f781548
African Hope <samuelguebo@gmail.com>
2017-09-28 13:45:58 +0000

    renoved the EqualizerHeight function from Js, using Css:flex instead

--- js/scripts.js
+++ js/scripts.js
@@ -25,16 +25,6 @@ jQuery(document).ready(function ($) {
         duration: 2000
     }, 50);
     
-    /* Equalizer */
-    $(window).load(function () {
-        /*
-        equalheight('.post-list .post-item');
-        equalheight('.post-list .post-item .post-item-caption');
-        */
-        equalheight('.category-row .post-item-title');
-        
-    });
-    
     /* Back to Top */
     if ($('#back-to-top').length) {
         var scrollTrigger = 700, // px
@@ -57,45 +47,5 @@ jQuery(document).ready(function ($) {
                 scrollTop: 0
             }, 700);
         });
-    }
-    
-    
-    /* 
-     * Thanks to CSS Tricks for pointing out this bit of jQuery
-     * http://css-tricks.com/equal-height-blocks-in-rows/
-     * 's been modified into a function called at page load and then each time the page 
-     * is resized. One large modification was to remove the set height before each new 
-     * calculation. 
-     */
-
-    function equalheight(container){
-
-    var currentTallest = 0,
-         currentRowStart = 0,
-         rowDivs = new Array(),
-         $el,
-         topPosition = 0;
-     $(container).each(function() {
-
-       $el = $(this);
-       $($el).height('auto')
-       topPostion = $el.position().top;
-
-       if (currentRowStart != topPostion) {
-         for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-           rowDivs[currentDiv].height(currentTallest);
-         }
-         rowDivs.length = 0; // empty the array
-         currentRowStart = topPostion;
-         currentTallest = $el.height();
-         rowDivs.push($el);
-       } else {
-         rowDivs.push($el);
-         currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-      }
-       for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-         rowDivs[currentDiv].height(currentTallest);
-       }
-     });
     }
 });
